bad9f58b5286142ef2a6ce6515ea751e7430443d
VyMission <89461105+VyMission@users.noreply.github.com>
2021-10-24 01:30:34 +0300

    Create exp.js

---
+++ exp.js
@@ -0,0 +1,11 @@
+let entire1 = ["Mallory", "Everest", "Mont Blanc", "Pillar Rock"];
+let entire2 = ["Mawson", "South Pole", "New Hebrides"];
+let entire3 = ["Hillary", "Everest", "South Pole"];
+
+let entire = [
+			  [entire1[1], entire3[0], entire1[0]],
+			  [entire3[2], entire3[0], entire2[0]],
+			  [entire1[2], entire1[0]],
+			  [entire1[3], entire1[0]],
+			  [entire2[2], entire2[0]],
+			   ];
